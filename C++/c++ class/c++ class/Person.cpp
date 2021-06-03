b14cd8f799070196bcd3e1bfd0b8bc9b1b1a2b96
kotercy9527 <31883359+kotercy9527@users.noreply.github.com>
2021-06-04 00:09:08 +0800

    创建第一个类，人类

---
+++ C++/c++ class/c++ class/Person.cpp
@@ -0,0 +1,8 @@
+//
+//  Person.cpp
+//  c++ class
+//
+//  Created by zhipeng jiang on 2021/6/4.
+//
+
+#include "Person.hpp"
---
+++ C++/c++ class/c++ class/Person.hpp
@@ -0,0 +1,13 @@
+//
+//  Person.hpp
+//  c++ class
+//
+//  Created by zhipeng jiang on 2021/6/4.
+//
+
+#ifndef Person_hpp
+#define Person_hpp
+
+#include <stdio.h>
+
+#endif /* Person_hpp */
